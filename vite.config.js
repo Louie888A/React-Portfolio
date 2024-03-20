4cc8364d6dcd6a6af97c49c7a5c6d20c9c7bade9
louie888a <alexlouie@ymail.com>
2024-03-20 10:07:08 +0000

    amended vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: '',
-=======
-  base: '/React-Portfolio/',
->>>>>>> 8972ea6e835eada505858aadeccb4c2290ad5383
+
+
   plugins: [react()],
 })
